7cd64f07adaf6bb33e836ebbdc89d55057ef2694
Tomoki <tomoki_ota@outlook.com>
2022-10-19 00:04:26 +0900

    fixed blurred navigation probelm, added about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,44 @@ const UIHamburgerBar = document.querySelector(
     ".navbar .mobile-only .hamburger .bar"
 );
 const UIMobileNav = document.querySelector(".mobile-nav");
-const UIBody = document.querySelector("body");
+const UIMain = document.querySelector("main");
+const UINavLinks = document.querySelectorAll(".nav-link");
+const UIHeader = document.querySelector("header");
+
 let showMenu = false;
 
 UIHamburgerBar.addEventListener("click", toggleMenu);
+window.addEventListener("scroll", addHeaderBg);
 
 function toggleMenu() {
     if (!showMenu) {
         UIHamburgerBar.classList.add("open");
         UIMobileNav.classList.add("open");
-        UIBody.classList.add("blur");
+        UIMain.classList.add("blur");
     } else {
         UIHamburgerBar.classList.remove("open");
         UIMobileNav.classList.remove("open");
-        UIBody.classList.remove("blur");
+        UIMain.classList.remove("blur");
     }
     showMenu = !showMenu;
+}
+
+// Closes Mobile Menu when link is clicked
+for (let i = 0; i < UINavLinks.length; i++) {
+    UINavLinks[i].addEventListener("click", function () {
+        UIHamburgerBar.classList.remove("open");
+        UIMobileNav.classList.remove("open");
+        UIMain.classList.remove("blur");
+        showMenu = false;
+    });
+}
+
+function addHeaderBg() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        UIHeader.style.background = "#1e293b";
+        UIHeader.classList.add("box-shadow");
+    } else {
+        UIHeader.style.background = "transparent";
+        UIHeader.classList.remove("box-shadow");
+    }
 }
